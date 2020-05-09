501c79b0bd26c0c2150eabdb5154cc55b98227b4
snjumaheshwari <snjumaheshwari@gmail.com>
2020-05-09 19:04:42 +0530

    contact with css

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -8,8 +8,18 @@ class ListContacts extends Component{
 			<ol className = 'contact-list' >
 				{
 					this.props.contacts.map ( contact => (
-						<li key = {contact.id}>
-							{contact.name}
+						<li key = {contact.id} className ='contact-list-item'>
+							<div className = 'contact-avatar' style =  {{ backgroundImage :`url(${contact.avatarURL})` }}>
+				
+							</div>
+							<div className = 'contact-details'>
+								<p> {contact.name}</p>
+								<p> {contact.handle}</p>
+							</div>
+
+							<button className = 'contact-remove'>
+								Remove
+							</button>
 						</li>))
 				}
 			</ol>
