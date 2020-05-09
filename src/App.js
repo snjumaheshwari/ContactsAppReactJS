01ef1624bce7b1473d95b7d5bbcc2fef4c70aed4
snjumaheshwari <snjumaheshwari@gmail.com>
2020-05-09 14:27:28 +0530

    Contacts ReactJs app Beginning

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class Persons extends Component{
+  render(){
+    const persons = this.props.persons;
+    const e4= <ol>
+      {persons.map (people => ( <li> {people.name} </li> )) }
+      </ol>
+      return e4;
+  }
+}
+class App extends Component {
+  render(){
   return (
     <div className="App">
+      <Persons   persons = {[
+      { 'name' : 'rohan'},
+      { 'name' : 'rohjjasans'}] }
+      />
+      <Persons   persons = {[
+      { 'name' : 'rohsssan'},
+      { 'name' : 'rohjjasans'}] }
+      />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reloadjssk.
         </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
+
         >
           Learn React
         </a>
       </header>
     </div>
   );
+}
 }
 
 export default App;
